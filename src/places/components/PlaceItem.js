2f2557904515d35bc324a9154e278a60fcb8c5ff
Anurag-Yoda <anurag.404@outlook.com>
2020-04-30 23:17:26 +0530

    complete till Modal

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,25 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./PlaceItem.css";
+import Button from "../../shared/components/FormElements/Button";
 import Card from "../../shared/components/UIElements/Card";
+import Modal from "../../shared/components/UIElements/Modal";
 const PlaceItem = (props) => {
+  const [showMap, setshowMap] = useState(false);
+  const openMapHandler = () => {
+    setshowMap(true);
+  };
+  const closeMapHandler = () => {
+    setshowMap(false);
+  };
+
   return (
-    <li className="place-item">
-      <Card className="place-item__content">
-        <div className="place-item__image">
-          <img src={props.image} alt={props.title} />
+    <React.Fragment>
+      <Modal
+        show={showMap}
+        onCancel={closeMapHandler}
+        header={props.header}
+        contentClass="place-item__modal-content"
+        footerClass="place-item__modal-actions"
+        footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
+      >
+        <div className="map-container">
+            <h2>the map</h2>
         </div>
-        <div className="place-item__info">
-          <h2> {props.title}</h2>
-          <h2> {props.address}</h2>
-          <h2> {props.description}</h2>
-        </div>
-        <div className="place-item__actions">
-          <button>VIEW ON MAP</button>
-          <button>EDIT</button>
-          <button>DELETE</button>
-        </div>
-      </Card>
-    </li>
+      </Modal>
+      <li className="place-item">
+        <Card className="place-item__content">
+          <div className="place-item__image">
+            <img src={props.image} alt={props.title} />
+          </div>
+          <div className="place-item__info">
+            <h2> {props.title}</h2>
+            <h2> {props.address}</h2>
+            <h2> {props.description}</h2>
+          </div>
+          <div className="place-item__actions">
+            <Button inverse onClick= {openMapHandler}>VIEW ON MAP</Button>
+            <Button to={`/places/${props.id}`}>EDIT</Button>
+            <Button danger>DELETE</Button>
+          </div>
+        </Card>
+      </li>
+    </React.Fragment>
   );
 };
 
