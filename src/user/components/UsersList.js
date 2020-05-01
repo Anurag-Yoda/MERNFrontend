20dffe0ed5740b993d6a862c8e5ec2f0e7152735
Anurag-Yoda <anurag.404@outlook.com>
2020-05-01 17:54:41 +0530

    complete frontend code

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,14 +1,15 @@
-import React from "react";
-import "./UsersList.css";
-import Card from "../../shared/components/UIElements/Card";
-import UserItem from "./UserItem";
+import React from 'react';
 
-const UsersList = (props) => {
+import UserItem from './UserItem';
+import Card from '../../shared/components/UIElements/Card';
+import './UsersList.css';
+
+const UsersList = props => {
   if (props.items.length === 0) {
     return (
       <div className="center">
         <Card>
-          <h2>No user found!</h2>
+          <h2>No users found.</h2>
         </Card>
       </div>
     );
@@ -16,17 +17,15 @@ const UsersList = (props) => {
 
   return (
     <ul className="users-list">
-      {props.items.map((user) => {
-        return (
-          <UserItem
-            key={user.id}
-            id={user.id}
-            image={user.image}
-            name={user.name}
-            placeCount={user.places}
-          />
-        );
-      })}
+      {props.items.map(user => (
+        <UserItem
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          placeCount={user.places}
+        />
+      ))}
     </ul>
   );
 };
